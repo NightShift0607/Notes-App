133332f31713657e2e9cadee058770b694c66d95
NightShift0607 <142142387+NightShift0607@users.noreply.github.com>
2024-04-22 12:10:10 +0530

    Completed Dashboard Home page with backend

--- server/controllers/dashboardControllers.js
+++ server/controllers/dashboardControllers.js
@@ -1,5 +1,13 @@
 const dashboardLayout = "../views/layouts/dashboard";
+const Note = require("../models/Notes");
+const mongoose = require("mongoose");
 
-exports.dashboardPage = (req, res) => {
-  res.render("dashboard/home", { layout: dashboardLayout });
+exports.dashboardPage = async (req, res) => {
+  try {
+    const user = req.user;
+    const notes = await Note.find({ user: user._id }).sort({ updatedAt: -1 });
+    res.render("dashboard/home", { layout: dashboardLayout, user, notes });
+  } catch (error) {
+    console.log("Error in Dashboard Page", error);
+  }
 };
