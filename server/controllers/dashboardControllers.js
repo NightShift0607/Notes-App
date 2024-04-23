f5d2dee6d899c2104dab391ae559e2215ca1d1e4
NightShift0607 <142142387+NightShift0607@users.noreply.github.com>
2024-04-23 12:19:28 +0530

    new-note and view-note page done

--- server/controllers/dashboardControllers.js
+++ server/controllers/dashboardControllers.js
@@ -1,12 +1,40 @@
 const dashboardLayout = "../views/layouts/dashboard";
 const Note = require("../models/Notes");
 
-exports.dashboardPage = async (req, res) => {
+async function getNotes(user) {
   try {
-    const user = req.user;
     const notes = await Note.find({ user: user._id }).sort({ updatedAt: -1 });
-    res.render("dashboard/home", { layout: dashboardLayout, user, notes });
+    return { notes, user };
   } catch (error) {
-    console.log("Error in Dashboard Page", error);
+    console.log("Error in Getting Notes", error);
+  }
+}
+
+// Dashboard page renderization
+exports.dashboardPage = async (req, res) => {
+  const { notes, user } = await getNotes(req.user);
+  res.render("dashboard/home", { layout: dashboardLayout, user, notes });
+};
+
+// Add note form rendering
+exports.newNotePage = async (req, res) => {
+  const { notes, user } = await getNotes(req.user);
+  res.render("dashboard/new-note", { layout: dashboardLayout, user, notes });
+};
+
+// View Note
+exports.viewNotePage = async (req, res) => {
+  const { notes, user } = await getNotes(req.user);
+  const noteId = req.params.id;
+  try {
+    const note = await Note.findById(noteId);
+    res.render("dashboard/note", {
+      layout: dashboardLayout,
+      user,
+      notes,
+      note,
+    });
+  } catch (error) {
+    console.log(error);
   }
 };
--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -4,5 +4,15 @@ const dashboardControllers = require("../controllers/dashboardControllers");
 const { checkAuthProceed } = require("../middleware/checkAuth");
 
 router.get("/dashboard", checkAuthProceed, dashboardControllers.dashboardPage);
+router.get(
+  "/dashboard/new-note",
+  checkAuthProceed,
+  dashboardControllers.newNotePage
+);
+router.get(
+  "/dashboard/note/:id",
+  checkAuthProceed,
+  dashboardControllers.viewNotePage
+);
 
 module.exports = router;
