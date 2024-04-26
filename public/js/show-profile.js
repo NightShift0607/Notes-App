136806cce2155bc002666b63670d465c02363dac
NightShift0607 <142142387+NightShift0607@users.noreply.github.com>
2024-04-26 14:29:10 +0530

    Bug fixes 3.0

--- public/js/show-profile.js
+++ public/js/show-profile.js
@@ -4,6 +4,16 @@ const profileClose = document.querySelector("#profile-close");
 const imageBtn = document.querySelector(".image-update label");
 const imageProfile = document.querySelector(".image-update img");
 const imageInput = document.querySelector("#profile-image");
+const userArea = document.querySelector(".user");
+const userOption = document.querySelector(".user-option");
+
+userArea.addEventListener("click", () => {
+  if (userOption.style.display === "none") {
+    userOption.style.display = "block";
+  } else {
+    userOption.style.display = "none";
+  }
+});
 
 profileBtn.addEventListener("click", () => {
   profileContainer.classList.toggle("open-profile");
